45277af5fe33bd2d98856f576842bb9270c4edd0
Blueishfiend692 <aditymakkar000@gmail.com>
2024-05-27 12:11:31 -0400

    add random walks

--- k-bandit.py
+++ k-bandit.py
@@ -11,16 +11,25 @@ upper = 5
 eps_number = 10
 eps = np.linspace(0, 1, eps_number)
 
-time_steps = 10000
+time_steps = 100000
 reward = np.zeros((eps_number+1, time_steps))
 reward[0] = np.arange(1, time_steps+1)
 
-q_star = get_array(k)
+
+q_star = np.zeros((time_steps, k))
+
+
+q_star[0] = get_array(k)
+Q_star = np.zeros((k,))
+
+
+for i in range(1, time_steps):
+  q_star[i] = q_star[i-1] + np.random.normal(loc=0.0, scale=0.01, size=(1,k))
+
 print(q_star)
-Q_star = get_array(k)
 
 for i in range(time_steps):
-  reward[0, i] = np.random.normal(loc=q_star[np.argmax(q_star)], scale=1.0, size = (1,))[0]
+  reward[0, i] = np.random.normal(loc=q_star[i, np.argmax(q_star[i])], scale=1.0, size = (1,))[0]
   if i > 0:
     reward[0, i] += reward[0, i-1]
     reward[0, i] = reward[0, i]
@@ -37,9 +46,9 @@ for e in range(eps_number):
 
   samples = np.random.choice(x, time_steps, p=x)
   for i in range(time_steps):
-    number = np.random.randint(0, len(Q_star)) if samples[i] == eps[e] else np.argmax(Q_star_current)
-    current_sample = np.random.normal(loc=q_star[number], scale=1.0, size = (1,))[0]
-    reward[e+1, i] = current_sample 
+    number = np.random.randint(0, len(Q_star_current)) if samples[i] == eps[e] else np.argmax(Q_star_current)
+    current_sample = np.random.normal(loc=q_star[i,number], scale=1.0, size = (1,))[0]
+    reward[e+1, i] = current_sample
 
     if i > 0:
       reward[e+1, i] += reward[e+1, i-1]
@@ -47,7 +56,7 @@ for e in range(eps_number):
 
     Q_star_estimate[number] += current_sample
     Q_star_divsor[number] += 1
-    Q_star_current[number] = Q_star_estimate[number]/Q_star_divsor[number]
+    Q_star_current[number] = Q_star_estimate[number] / 10
 
 
 
