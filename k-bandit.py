65210537ed0d823a43a74adb870d739c014fcabc
Blueishfiend692 <aditymakkar000@gmail.com>
2024-06-02 21:25:48 -0400

    policy iteration

--- k-bandit.py
+++ k-bandit.py
@@ -4,14 +4,14 @@ import random as r
 
 def get_array(k):
   return np.random.random((k,))
-k = 10
+k = 100
 lower = 0
 upper = 5
 
 eps_number = 10
 eps = np.linspace(0, 1, eps_number)
 
-time_steps = 100000
+time_steps = 1000000
 reward = np.zeros((eps_number+1, time_steps))
 reward[0] = np.arange(1, time_steps+1)
 
@@ -20,11 +20,11 @@ q_star = np.zeros((time_steps, k))
 
 
 q_star[0] = get_array(k)
-Q_star = np.zeros((k,))
+Q_star = np.ones((k,)) * 5
 
 
 for i in range(1, time_steps):
-  q_star[i] = q_star[i-1] + np.random.normal(loc=0.0, scale=0.01, size=(1,k))
+  q_star[i] = q_star[i-1] + np.random.normal(loc=0.0, scale=1, size=(1,k))
 
 print(q_star)
 
@@ -57,6 +57,7 @@ for e in range(eps_number):
     Q_star_estimate[number] += current_sample
     Q_star_divsor[number] += 1
     Q_star_current[number] = Q_star_estimate[number] / 10
+    
 
 
 
